7da56805fb53eeadde9a46f7e334e40bde3bf2ea
anarcher <anarcher@gmail.com>
2015-05-13 00:59:52 +0900

    :construction: :white_check_mark: #2 TestTaskRunnerHTTPGet

--- worker/taskrunner_test.go
+++ worker/taskrunner_test.go
@@ -1,9 +1,12 @@
 package worker
 
 import (
+	"fmt"
 	"github.com/seanpont/assert"
 	"golang.org/x/net/context"
 	"gopkg.in/loom.v1/worker/config"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -30,13 +33,57 @@ func TestNewTaskRunner(t *testing.T) {
 }
 
 func TestTaskRunnerHTTPGet(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer ts.Close()
+
 	a := assert.Assert(t)
 	ctx := context.Background()
 	task := &config.Task{
 		Name: "helloworld",
 		HTTP: &config.HTTP{
-			URL:    "http://example.com/",
+			URL:    ts.URL,
 			Method: "GET",
+			Data: map[string]string{
+				"p1": "1",
+				"p2": "2",
+			},
+		},
+	}
+	jobConfig := &config.Job{}
+	jobConfig.Tasks = append(jobConfig.Tasks, task)
+
+	job := NewJob(ctx, "id", jobConfig)
+	tr := NewTaskRunner(job, task)
+
+	tr.Run()
+
+	<-job.ctx.Done()
+
+	a.Equal(tr.fsm.Current(), "DONE")
+	a.Equal(tr.State(), "DONE")
+	t.Logf("task.output:%v", tr.Output())
+
+}
+
+func TestTaskRunnerHTTPPost(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.NotFound(w, r)
+			return
+		}
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer ts.Close()
+
+	a := assert.Assert(t)
+	ctx := context.Background()
+	task := &config.Task{
+		Name: "helloworld",
+		HTTP: &config.HTTP{
+			URL:    ts.URL,
+			Method: "POST",
 			Data: map[string]string{
 				"p1": "1",
 				"p2": "2",
