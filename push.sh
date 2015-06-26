42a0ef95cdfd77f37156683058023b2ffd17e1b3
msshin <anarcher@gmail.com>
2015-06-26 16:24:27 +0900

    Add circle.yml and push.sh

---
+++ push.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+SHA1=$1
+
+#Check this commit is tagging
+TAG=`git name-rev --name-only --tags $SHA1`
+
+if [ ! $TAG == 'undefined' ] ; then
+
+docker tag goloom/loom:$SHA1 goloom/loom:$TAG
+docker push goloom/loom:$TAG 
+
+#docker build -t goloom/loom:$TAG-ubuntu14.04 -f Dockerfile.ubuntu14.04 . 
+#docker push goloom/loom:$TAG-ubuntu14.04
+
+fi
+
+
