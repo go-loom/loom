b125035f3c3c0fe5114768207f09fed0b2dc1e39
msshin <anarcher@gmail.com>
2015-08-07 16:13:52 +0900

    Change push.sh

--- push.sh
+++ push.sh
@@ -8,13 +8,16 @@ set -e
 #Check this commit is tagging
 TAG=`git name-rev --name-only --tags $SHA1`
 
+echo $TAG
+
 if [ ! $TAG == 'undefined' ] ; then
 
+echo "Building and Pushing images"
 docker tag goloom/loom:$SHA1 goloom/images:$TAG
 docker push goloom/images:$TAG 
 
-#docker build -t goloom/loom:$TAG-ubuntu14.04 -f Dockerfile.ubuntu14.04 . 
-#docker push goloom/loom:$TAG-ubuntu14.04
+docker build -t goloom/loom:$TAG-ubuntu14.04 -f Dockerfile.ubuntu14.04 . 
+docker push goloom/loom:$TAG-ubuntu14.04
 
 fi
 
